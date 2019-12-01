6ad632fd2ae86dec6abb9c9d01fe8dd4ef00c36d
Nardo <alejandronrd0@gmail.com>
2019-12-01 15:54:55 +0100

    data model code added to the project

---
+++ ServerOAuthModule/ClassDomain.Context/DataContext.cs
@@ -0,0 +1,31 @@
+﻿using CodeDomainClasses;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeDomain.DataModel
+{
+    public class CodesContext : DbContext
+    {
+        public DbSet<Code> Codes { get; set; }
+
+        public CodesContext() : base()
+        {
+
+        }
+        public CodesContext(DbContextOptions<CodesContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builderOptions)
+        {
+            if (!builderOptions.IsConfigured)
+            {
+                builderOptions.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Codes;Trusted_Connection=True");
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Code>().HasIndex(x => x.state);
+        }
+    }
+}
---
+++ ServerOAuthModule/DataModel.Classes/Classes.cs
@@ -0,0 +1,12 @@
+﻿namespace CodeDomainClasses
+{
+    public class Code
+    {
+        public string code { get; set; }
+
+        public string state { get; set; }
+        public string secret_key { get; set; }
+
+        public int Id { get; set; }
+    }
+}
